af3e8dc7e4ec06a8fc471007e5cff12e209f25e0
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 22:15:21 +0530

    reverse.cs

---
+++ csharp/reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace program
+{
+    class reverse
+    {
+        public static void Main()
+        {
+            char b, a, t;
+            Console.WriteLine(" enter 1st");
+            b = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(" enter 2nd");
+            a = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(" enter 3rd");
+            t = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("reverse order:" + t + a + b);
+            Console.ReadKey();
+        }
+    }
+}
+
