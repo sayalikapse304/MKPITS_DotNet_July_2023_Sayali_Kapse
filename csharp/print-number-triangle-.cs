c6b593eabb2345467e970fc52b1f3f17ba7c4d20
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 22:14:37 +0530

    print-triangle.cs

---
+++ csharp/print-number-triangle-.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace program
+{
+class operation
+{
+    public static void Main()
+    {
+
+        float num1, num2, res;
+        char op;
+        Console.WriteLine("enter first number");
+        num1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("enter op ");
+            op = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("enter seconde number");
+        num2 = Convert.ToSingle(Console.ReadLine());
+        
+        res = num1 + num2;
+            res = num1 -num2;
+            Console.WriteLine(+res1  );
+            
+
+            Console.ReadKey();
+    }
+}
+}
+        
+
+
+
+
+
+
+
+
+
+
+
---
+++ csharp/print-triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace program
+{
+    class triangle
+    {
+        public static void Main()
+        {
+            int num, width;
+            Console.WriteLine("enter num");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(666666);
+            Console.WriteLine(66666);
+            Console.WriteLine(6666);
+            Console.WriteLine(666);
+            Console.WriteLine(66);
+            Console.WriteLine(6);
+            Console.ReadKey();
+        }
+    }
+}
+            
+
