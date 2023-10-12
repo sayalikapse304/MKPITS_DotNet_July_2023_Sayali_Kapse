d9f1e8e50a155f2021342cc5165aa8e404cc4d3d
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 22:11:32 +0530

    hello-name.cs

---
+++ csharp/hello-name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace program
+{
+    class name
+    {
+        public static void Main()
+        {
+            string name;
+            Console.WriteLine("Hellow");
+            Console.WriteLine("name");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello:"+name);
+            Console.ReadLine();
+        }
+    }
+}
