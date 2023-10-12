177bcec3779b3df66e983c0768461c809b5f638f
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 22:13:01 +0530

    print-hello.cs

---
+++ csharp/print-hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            //console is a class
+            //writeline is a method
+            Console.ReadLine();
+        }
+    }
+}
+
+
