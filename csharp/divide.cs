e9ac44ad31972f51ffd8a8cf2a9a8c69624c2eff
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 21:59:48 +0530

    divide.cs

---
+++ csharp/divide.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace divide
+{
+    class div
+    {
+        public static void Main()
+        {
+            float num1, num2;
+            float res;
+            Console.WriteLine("enetr num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enetr num2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            res = num1/ num2;
+            Console.WriteLine("Result=" + res);
+            Console.ReadKey();
+        }
+    }
+}
