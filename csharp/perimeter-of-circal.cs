6a3d5387cad0cf6695fae6a0db854e3ba2564de4
sayali kapse <sayalikapse53@gmail.com>
2023-10-12 22:12:14 +0530

    perimeter-of-circal.cs

---
+++ csharp/perimeter-of-circal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace area
+{
+    class circal
+    {
+        public static void Main()
+        {
+            float radius, aoc, perimeter;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToSingle(Console.ReadLine());
+
+            perimeter = 2 * 3.14f * radius;
+            Console.WriteLine("perimeter of circal=" + perimeter);
+
+            aoc = 3.14f * radius * radius;
+            Console.WriteLine("Area of circal=" + aoc);
+
+            Console.ReadKey();
+        }
+    }
+}
+        
