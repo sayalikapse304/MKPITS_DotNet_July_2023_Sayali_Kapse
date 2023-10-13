e3f84cd99f6f68276126acb51d4912f9f62f7004
sayali kapse <sayalikapse53@gmail.com>
2023-10-13 22:16:09 +0530

    sphere.cs

---
+++ csharp/sphere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace program
+{
+    class sphere
+    {
+        public static void Main()
+        {
+            float r, surface, volume;
+            Console.WriteLine("enter radius");
+            r = Convert.ToSingle(Console.ReadLine());
+            surface = 4 * 3.14f * r * r;
+            volume = 4 / 3 * 3.14f * r * r * r;
+            Console.WriteLine("surface of sphere=" + surface);
+            Console.WriteLine("volume of sphere=" + volume);
+            Console.ReadKey();
+        }
+    }
+}
+
