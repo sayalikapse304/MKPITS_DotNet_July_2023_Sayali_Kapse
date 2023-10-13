849c22832a7f405a123b6e93ae838c53a05e3c18
sayali kapse <sayalikapse53@gmail.com>
2023-10-13 22:17:40 +0530

    square-num.cs

---
+++ csharp/square-num.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace squareprogram
+{
+    class square
+    {
+        public static void Main()
+        {
+            int num, sqr;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            sqr = num * num;
+            Console.WriteLine("squar=" + sqr);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+   
