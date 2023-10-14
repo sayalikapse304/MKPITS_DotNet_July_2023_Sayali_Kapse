f835b96b28c5881f3bf3dda8a1c21f182f0cfc72
sayali kapse <sayalikapse53@gmail.com>
2023-10-14 18:12:28 +0530

    cost-price-selling-price.cs

---
+++ csharp/cost-price-selling-price.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace program
+{
+    class price
+    {
+        public static void Main()
+        {
+            int c, s, res;
+            Console.WriteLine("Enter cost price");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter selling price");
+            s= Convert.ToInt32(Console.ReadLine());
+            if(s>c)
+            {
+                res = s - c;
+                Console.WriteLine("You can book your profit amount :" + res);
+            }
+            else
+            {
+                res = c - s;
+                Console.WriteLine("You can book your loss amount :" + res);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
