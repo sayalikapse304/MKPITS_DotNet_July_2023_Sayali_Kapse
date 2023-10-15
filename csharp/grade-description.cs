71623f410e6ba97b7cc43d57653751f302636726
sayali kapse <sayalikapse53@gmail.com>
2023-10-15 10:51:50 +0530

    grade-description.cs

---
+++ csharp/grade-description.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace program
+{
+    class grade
+    {
+        public static void Main()
+        {
+            char grd;
+            String res;
+            Console.WriteLine("grade");
+             grd=Convert.ToChar (Console.ReadLine());
+
+
+            if (grd == 'E')
+            {
+                res = "Excellent";
+            }
+                
+            else if (grd=='V')
+            {
+                res = "Very Good"; 
+
+            }
+            else if(grd=='G')
+            {
+                res = "Good";
+            }
+            else
+            {
+                res = "fail";
+            }
+            Console.WriteLine("you have choosen:"+res);
+            Console.ReadKey();
+        }
+    }
+}
