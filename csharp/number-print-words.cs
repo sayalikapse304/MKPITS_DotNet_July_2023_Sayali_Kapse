0dc43b33b564cc8020b5b82cf2429110b672472d
sayali kapse <sayalikapse53@gmail.com>
2023-10-15 10:53:34 +0530

    number-print-words.cs

---
+++ csharp/number-print-words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace program
+{
+    class number
+    {
+        public static void Main()
+        {
+            int digit;
+            Console.WriteLine("Enter digit");
+            digit = Convert.ToInt32(Console.ReadLine());
+
+            if (digit == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (digit == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (digit == 3)
+            {
+                Console.WriteLine("Three");
+            }
+            else
+            {
+                Console.WriteLine("Invalid digit");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+        
