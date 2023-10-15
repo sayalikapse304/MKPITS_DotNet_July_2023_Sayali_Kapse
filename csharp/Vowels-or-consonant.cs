c1e90fdc4c6b2410995d582fdc5a3c9b8c3aca17
sayali kapse <sayalikapse53@gmail.com>
2023-10-15 10:51:02 +0530

    Vowels-or-consonant.cs

---
+++ csharp/Vowels-or-consonant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace program
+{
+    class vowels
+    {
+        public static void Main()
+        {
+            char ch;
+            Console.WriteLine("Enter the Charachter ");
+            ch = Convert.ToChar(Console.ReadLine());
+            
+            if (ch == 'a' ||  ch == 'e' ||ch == 'i' ||  ch == 'o' ||  ch == 'u' )
+            {
+                Console.WriteLine("It is a vowel" );
+            }
+            else
+            {
+                Console.WriteLine("It is consonant");
+            }
+            Console.ReadKey();
+        }
+    }
+} 
+       
