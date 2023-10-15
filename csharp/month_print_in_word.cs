c0519bbc77190a6b49180664fc5e4851b40613bf
sayali kapse <sayalikapse53@gmail.com>
2023-10-15 10:52:34 +0530

    month_print_in_word.cs

---
+++ csharp/month_print_in_word.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace program
+{
+    class number
+    {
+        public static void Main()
+        {
+            int digit;
+            Console.WriteLine("Enter digit");
+            digit = Convert.ToInt32(Console.ReadLine());
+
+            if (digit == 1)
+            {
+                Console.WriteLine("Janwary");
+            }
+            else if (digit == 2)
+            {
+                Console.WriteLine("Februawary");
+            }
+            else if (digit == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Month");
+            }
+            Console.ReadKey();
+        }
+    }
+}
