8a3e498e28bccda5bb8632a1cb7df8ade03f583e
sayali kapse <sayalikapse53@gmail.com>
2023-11-05 21:04:08 +0530

    .

---
+++ csharp/method-overloadding-book-information.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    class Book
+    {
+        int bookid;
+        String title, author;
+        int price;
+
+        public void getdata()
+        {
+            bookid = 1;
+            title = "oracle";
+            author = "james";
+            price = 200;
+
+        }
+        
+        public void getdata(int bookid, String title, String author, int price)
+        {
+            this.bookid = bookid;
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine("book id = " + bookid);
+            Console.WriteLine("book title = " + title);
+            Console.WriteLine("book author= " + author);
+            Console.WriteLine("price = " + price);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.getdata();
+            b.display();
+            Console.WriteLine("-----------------book1 details ----------");
+            Book b1 = new Book();
+            b1.getdata(111, "csharp", "martin", 2000);
+            b1.display();
+
+            Console.WriteLine("-----------------book1 details ----------");
+            Console.WriteLine("enter bookid");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter title");
+            String title = Console.ReadLine();
+            Console.WriteLine("enter author");
+            String author = Console.ReadLine();
+            Console.WriteLine("enter price");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-----------------book2 details ----------");
+            Book b2 = new Book();
+            b2.getdata(id, title, author, price);
+            b2.display();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ csharp/method-overloaded-zero-one-two-three-four-parameters.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    class Book
+    {
+        int bookid;
+        String title, author;
+        int price;
+
+        public void getdata()
+        {
+            bookid = 1;
+            title = "power of mind";
+            author = "santosh";
+            price = 500;
+
+        }
+        //method overloaded 
+        public void getdata(int bookid)
+        {
+            this.bookid = bookid;
+
+        }
+        public void getdata(int bookid, String title)
+        {
+            this.bookid = bookid;
+            this.title = title;
+
+        }
+        public void getdata(int bookid, String title, String author)
+        {
+            this.bookid = bookid;
+            this.title = title;
+            this.author = author;
+
+        }
+        public void getdata(int bookid, String title, String author, int price)
+        {
+            this.bookid = bookid;
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void display()
+        {
+            Console.WriteLine("book id = " + bookid);
+            Console.WriteLine("book title = " + title);
+            Console.WriteLine("book author= " + author);
+            Console.WriteLine("price = " + price);
+
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.getdata();
+            b.display();
+            Console.WriteLine("-----------------book1 details ----------");
+            Book b1 = new Book();
+            b1.getdata(2, "csharp");
+            b1.display();
+
+            Console.WriteLine("-----------------book1 details ----------");
+            Book b2 = new Book();
+            b2.getdata(111, "csharp", "sayali");
+            b2.display();
+
+            Console.WriteLine("-----------------book1 details ----------");
+
+            Book b3 = new Book();
+            b3.getdata(111, "csharp", "payal", 444);
+            b3.display();
+
+            Console.WriteLine("-----------------book1 details ----------");
+
+            Console.ReadLine();
+
+        }
+    }
+}
