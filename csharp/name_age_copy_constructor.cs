f12deaea48b4906acf05b0acca8e1af8a666488e
sayali kapse <sayalikapse53@gmail.com>
2023-11-05 19:41:19 +0530

    .

---
+++ csharp/name_age_copy_constructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace program
+{
+    class employee
+    {
+        private string name;
+        private int age;
+        public employee(employee emp)   // declaring Copy constructor.
+        {
+            name = emp.name;
+            age = emp.age;
+        }
+        public employee(string name, int age)  // Instance constructor.
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Details     // Get deatils of employee
+        {
+            get
+            {
+                return " The age of " + name + " is " + age.ToString();
+            }
+        }
+    }
+    class empdetail
+    {
+        static void Main()
+        {
+            employee emp1 = new employee("sayali", 23);  // Create a new employee object.
+            employee emp2 = new employee(emp1);          // here is emp1 details is copied to emp2.
+            Console.WriteLine(emp2.Details);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/static_constructor.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+namespace program
+    public class employee
+    {
+        static employee() // Static constructor
+        declaration{Console.WriteLine("The static constructor ");
+    }
+    public static void Salary()
+    {
+        Console.WriteLine();
+        Console.WriteLine("The Salary method");
+    }
+}
+class details
+{
+    static void Main()
+    {
+        Console.WriteLine("----------Static constrctor example by vithal wadje------------------");
+        Console.WriteLine();
+        employee.Salary();
+        Console.ReadLine();
+    }
+}
+}
