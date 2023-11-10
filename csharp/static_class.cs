2d26621e314729568a93570147665a11dc97b6ae
sayali kapse <sayalikapse53@gmail.com>
2023-11-10 09:14:41 +0530

    .

---
+++ csharp/static_class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace program
+{
+    public static class MyMath
+    {
+        public static float PI = 3.14f;
+        public static int cube(int n) { return n * n * n; }
+    }
+    class TestMyMath
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Value of PI is: " + MyMath.PI);
+            Console.WriteLine("Cube of 3 is: " + MyMath.cube(3));
+        }
+    }
+}
--- csharp/static_constructor.cs
+++ csharp/static_constructor.cs
@@ -1,25 +1,43 @@
-﻿
-using System;
-namespace program
-    public class employee
-    {
-        static employee() // Static constructor
-        declaration{Console.WriteLine("The static constructor ");
-    }
-    public static void Salary()
-    {
-        Console.WriteLine();
-        Console.WriteLine("The Salary method");
-    }
-}
-class details
+﻿using System;
+class program
 {
-    static void Main()
+    public class Account
     {
-        Console.WriteLine("----------Static constrctor example by vithal wadje------------------");
-        Console.WriteLine();
-        employee.Salary();
-        Console.ReadLine();
+        public int id;
+        public String name;
+        public static float rateOfInterest;
+        public Account(int id, String name)
+        {
+            Console.WriteLine("instance constructor is called ");
+            this.id = id;
+            this.name = name;
+        }
+        static Account()
+        {
+            Console.WriteLine("static constructor is called ");
+            rateOfInterest = 9.5f;
+        }
+        public void display()
+        {
+
+            Console.WriteLine(id + " " + name + " " + rateOfInterest);
+        }
     }
-}
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Account a1 = new Account(101, "Sonoo");
+            Account a2 = new Account(102, "Mahesh");
+            a1.display();
+            a2.display();
+
+
+
+
+        }
+    }
 }
