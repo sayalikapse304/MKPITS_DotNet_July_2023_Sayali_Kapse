5130dcbdabf508b36e0227bc7c8473ccab3b9dee
sayali kapse <sayalikapse53@gmail.com>
2023-12-03 14:15:50 +0530

    .

---
+++ csharp/accept-num-to-that-num-using-for-window.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace accept_number_to_that_num_using_for_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Number n = new Number();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num=Convert.ToInt32(textBox1.Text);
+            n.getdata(num);
+            label2.Text = n.res;
+            
+        }
+    }
+}
---
+++ csharp/leap-year-window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace leap_year_or_not_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Year y=new Year();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year=Convert.ToInt32(textBox1.Text);
+            y.getdata(year);
+            label2.Text = y.res;
+        }
+    }
+}
