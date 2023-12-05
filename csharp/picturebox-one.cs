74439b8f01541da71477482f57576f1181a5a29d
sayali kapse <sayalikapse53@gmail.com>
2023-12-05 17:05:10 +0530

    .

---
+++ csharp/picturebox-one.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picture_boxxxxxxxxxxxxxxxxxxx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string imgname = comboBox1.Text;
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.Value = i;
+            }
+            pictureBox1.Image = System.Drawing
+                .Image.FromFile(imgname);
+
+        }
+    }
+}
---
+++ csharp/sum-of-odd-num.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sum_of_odd_natural_num_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Odd o=new Odd();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num=Convert.ToInt32(textBox1.Text);
+           string res= o.display(num);
+            label2.Text ="sum= "+res;
+        }
+    }
+}
