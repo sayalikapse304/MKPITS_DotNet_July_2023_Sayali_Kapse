2291489e413f7d0ff968691596dc9a3c5c2ac8d4
sayali kapse <sayalikapse53@gmail.com>
2023-12-11 17:59:17 +0530

    .

---
+++ csharp/animal-project.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound");
+    }
+}
+
+class Mammal : Animal
+{
+    public Mammal(string name, int age) : base(name, age)
+    {
+    }
+
+    // Additional properties or methods specific to mammals
+}
+
+class Reptile : Animal
+{
+    public Reptile(string name, int age) : base(name, age)
+    {
+    }
+
+    // Additional properties or methods specific to reptiles
+}
+
+class Bird : Animal
+{
+    public Bird(string name, int age) : base(name, age)
+    {
+    }
+
+    // Additional properties or methods specific to birds
+}
+
+class Lion : Mammal
+{
+    public Lion(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Roar!");
+    }
+
+    // Additional properties or methods specific to lions
+}
+
+class Snake : Reptile
+{
+    public Snake(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Hiss!");
+    }
+
+    // Additional properties or methods specific to snakes
+}
+
+class Eagle : Bird
+{
+    public Eagle(string name, int age) : base(name, age)
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine("Screech!");
+    }
+
+    // Additional properties or methods specific to eagles
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create instances of different animal types
+        Lion lion = new Lion("Simba", 5);
+        Snake snake = new Snake("Nagini", 3);
+        Eagle eagle = new Eagle("Thunder", 2);
+
+        // Demonstrate polymorphism by calling the common method on objects of different derived classes
+        Animal[] animals = { lion, snake, eagle };
+
+        foreach (var animal in animals)
+        {
+            Console.WriteLine($"Name: {animal.Name}, Age: {animal.Age}");
+            animal.MakeSound();
+           
+        }
+        Console.WriteLine();
+    }
+}
---
+++ csharp/library-project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace program
+{
+    class Author
+    {
+        public string AuthorName { get; set; }
+
+        public Author(string authorName)
+        {
+            AuthorName = authorName;
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public Author Author { get; set; }
+
+        public Book(string title, string isbn, Author author)
+        {
+            Title = title;
+            ISBN = isbn;
+            Author = author;
+        }
+
+    }
