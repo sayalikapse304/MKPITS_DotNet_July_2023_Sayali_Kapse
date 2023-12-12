f8e53e091f74cf4a76db107b35c43e01c978e68a
sayali kapse <sayalikapse53@gmail.com>
2023-12-12 22:56:32 +0530

    .

---
+++ csharp/lower-triangular-matrix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        int x;
+        public static void Main(string[] args)
+        {
+            int m, n, i, j;
+
+            Console.Write("Enter Number Of Rows And Columns Of Matrices A and B : ");
+            m = Convert.ToInt16(Console.ReadLine());
+            n = Convert.ToInt16(Console.ReadLine());
+            int[,] A = new int[10, 10];
+            Console.Write("\nEnter The First Matrix : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    A[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("\nMatrix A : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(A[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n Setting Zero to illustrate " +
+                              "Lower Triangular Matrix\n");
+            for (i = 0; i < m; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < 3; j++)
+                {
+                    if (i >= j)
+                        Console.Write(A[i, j] + "\t");
+                    else
+                        Console.Write("0\t");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
