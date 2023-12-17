cd00eedaa3e84b3ce5daa1d3c90db87ff5dae073
sayali kapse <sayalikapse53@gmail.com>
2023-12-17 20:07:11 +0530

    .

---
+++ csharp/convert-uppercase-to-lowercase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string str;
+        Console.WriteLine("Enter the String in Uppercase :");
+        str = Console.ReadLine();
+        Console.WriteLine("String in LowerCase : {0}", str.ToLower());
+        Console.ReadLine();
+    }
+}
