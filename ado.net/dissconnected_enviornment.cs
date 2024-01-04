7a26b629b348a7ba7361c55cdc8ac5681206c41e
sayali kapse <sayalikapse53@gmail.com>
2024-01-04 14:26:19 +0530

    .

---
+++ ado.net/dissconnected_enviornment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dissconnected_enviornment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("server=.\\SQLEXPRESS;integrated security=true;database=sample11");
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        SqlCommandBuilder cb = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("select * from student2", con);
+            cb = new SqlCommandBuilder(da);
+            
+            da.Fill(ds, "stud");
+            dataGridView1.DataSource = ds.Tables["stud"];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                da.Update(ds.Tables["stud"]);
+                MessageBox.Show("Record updated");
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.ToString());
+            }
+        }
+    }
+    
+}
---
+++ ado.net/login_form_with_menustrip.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace login_form_with_menustrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string constr = "server=LAPTOP-LP6VHOMO\\SQLEXPRESS; integrated security = true; database = sample11";
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(constr);
+            string qr = "select count(*) from Login22 where username=@username and password=@password";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(qr, con);
+                cmd.Parameters.AddWithValue("@username", textBox1.Text);
+                cmd.Parameters.AddWithValue("@password", textBox2.Text);
+                con.Open();
+                int cnt=Convert.ToInt32(cmd.ExecuteScalar());
+                if(cnt>0)
+                {
+                    MessageBox.Show("Login Successfull");
+                    panel1.Visible = false;
+                    menuStrip1.Visible = true;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Credentails! try Again");
+                }
+                
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.ToString());
+            }
+            
+        }
+    }
+}
+
+
+
+
+           
+     
