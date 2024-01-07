7e21e7679f2773e69061c72237f37e0aa531b4a8
sayali kapse <sayalikapse53@gmail.com>
2024-01-07 12:13:39 +0530

    .

---
+++ csharp/calculate_distance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class program
+{
+    public static void Main()
+    {
+        int speed, distance, time;
+        Console.WriteLine("Enter the Speed(km/hr) : ");
+        speed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Time(hrs) : ");
+        time = Convert.ToInt32(Console.ReadLine());
+        distance = speed * time;
+        Console.WriteLine("Distance Travelled (kms) : " + distance);
+        Console.ReadLine();
+    }
+}
