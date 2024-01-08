e71f46f31fc8a63ecad8defee6a8cb98c8a9c962
sayali kapse <sayalikapse53@gmail.com>
2024-01-08 21:05:20 +0530

    .

---
+++ csharp/calculate_accelaration.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        int v, t, acc;
+        Console.WriteLine("Enter the Velocity : ");
+        v = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the Time : ");
+        t = int.Parse(Console.ReadLine());
+        acc = v / t;
+        Console.WriteLine("Acceleration : {0}", acc);
+    }
+}
