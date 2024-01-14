abf224d7def5611189081f8091119f357fb0bec9
sayali kapse <sayalikapse53@gmail.com>
2024-01-14 14:03:37 +0530

    .

---
+++ csharp/concnating_two_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string s1 = "Good";
+        string s2 = "Morning";
+        string s3 = string.Concat(s1, s2);
+        Console.WriteLine(s3);
+        Console.ReadLine();
+    }
+}
