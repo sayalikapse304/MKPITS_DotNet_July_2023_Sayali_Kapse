28673b944e6e4f01b00da18d50a46ee5591cc7a0
sayali kapse <sayalikapse53@gmail.com>
2024-01-15 11:31:39 +0530

    .

---
+++ csharp/sum_of_first_fifteen_natural_number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class program
+{
+    public static void Main()
+    {
+        int num, sum = 0;
+        for (num = 1; num <= 50; num++)
+        {
+            sum = sum + num;
+        }
+        Console.WriteLine("Sum = {0}", sum);
+        Console.ReadLine();
+    }
+}
