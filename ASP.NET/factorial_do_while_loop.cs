83007486de462602f7c8a4c12a0dd884a4c30c4e
sayali kapse <sayalikapse53@gmail.com>
2024-01-20 22:41:17 +0530

    .

---
+++ ASP.NET/factorial_do_while_loop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace for_loop_factorial
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int fact = 1;
+            int num = Convert.ToInt32(TextBox1.Text);
+            do
+            {
+                fact = fact * num;
+                num--;
+            }
+            while (num > 0);
+            
+
+
+                Label1.Text = "Factorial of number=" + fact;
+            
+
+        }
+    }
+}
